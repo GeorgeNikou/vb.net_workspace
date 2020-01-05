238ce0967cfc3f19306f847d466a0fbe1637aee2
GeorgeNikou <george.nikou05@gmail.com>
2020-01-04 22:40:20 -0500

    *

--- mod4lab2-final/Module4/module4/My Project/Application.Designer.vb
+++ mod4lab2-final/Module4/module4/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.module4.lab2
+            Me.MainForm = Global.module4.lab1_b
         End Sub
     End Class
 End Namespace
